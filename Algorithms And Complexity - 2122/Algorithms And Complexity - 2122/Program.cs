eb7bda092afdc78167dfac51d04dc8213ab4fe69
Alexander Elwell <alexander.hyaden.elwell@gmail.com>
2022-04-23 00:05:36 +0100

    nightly save

--- Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Program.cs
+++ Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Program.cs
@@ -8,26 +8,33 @@ using Algorithms_And_Complexity___2122;
 var output = Input.FileReturn();
 
 // //Printing the output of the FileReturn()
-// foreach (var i in output)
-// {
-//     Console.WriteLine(i);
-//     // checking for the returned values  
-//     // foreach (var x in i.Value)
-//     // {
-//     //     Console.WriteLine(x);
-//     // }
-// }
+foreach (var i in output)
+{
+    Console.WriteLine(i);
+    // checking for the returned values  
+    // foreach (var x in i.Value)
+    // {
+    //     Console.WriteLine(x);
+    // }
+}
 
-
-//TODO - get the user's input on file.
 
 int count = 0;
 Console.WriteLine("Please Choose A file:");
+List<string>? keyList = null;
 foreach (var i in output)
 {
     Console.WriteLine(count + " " + i.Key);
+    //Conditional Access to prevent null not being loaded - error handling / preventing errors from occuring
+    keyList?.Add(i.Key);
     count++;
 }
 string? option = Console.ReadLine();
-
-//TODO - make this use a list and then index the list to get to the key value to access the dictionary. 
+//if option is equal to the length of list or smaller, continue
+if (keyList != null && option != null && int.Parse(option) <= keyList.Count())
+{
+    string valueToUse = keyList[int.Parse(option)];
+    
+    
+    //TODO - save the key and value that the user has chosen and move on to sorting.
+}
