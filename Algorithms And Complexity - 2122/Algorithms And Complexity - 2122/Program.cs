e2f6f16fa98f1c2a0ece484951869e484aa9930b
Alexander Elwell <alexander.hyaden.elwell@gmail.com>
2022-04-23 18:01:21 +0100

    Input Complete

--- Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Program.cs
+++ Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Program.cs
@@ -1,40 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 //Setting the workspace to get classes from
+
 using Algorithms_And_Complexity___2122;
-//
-// //Creating a new instance of input to get the file's needed. 
-// var input = new Input();
-//Calling FileReturn and saving the output as 'output' (List)
+
+// Calling FileReturn and saving the output as 'output' (Dictionary)
 var output = Input.FileReturn();
 
-// //Printing the output of the FileReturn()
+// Printing the output of the FileReturn()
 foreach (var i in output)
 {
     Console.WriteLine(i);
+    
     // checking for the returned values  
+    
     // foreach (var x in i.Value)
     // {
     //     Console.WriteLine(x);
     // }
 }
 
-
+//Count for the amount of data 
 int count = 0;
+//Outputting instructions to User Terminal
 Console.WriteLine("Please Choose A file:");
-List<string>? keyList = null;
+//Creating new List that will hold all of the Key's we can use to access the dictionary
+List<string> keyList = new List<string>();
+//Loop through the Dictionary from FileReturn (output) 
 foreach (var i in output)
 {
+    //Output the options to the user
     Console.WriteLine(count + " " + i.Key);
-    //Conditional Access to prevent null not being loaded - error handling / preventing errors from occuring
-    keyList?.Add(i.Key);
+    //Adding the key to to the KeyList to allow us to access Dictionary
+    keyList.Add(i.Key);
+    //Adding to the count / the index of keyList
     count++;
 }
-string? option = Console.ReadLine();
-//if option is equal to the length of list or smaller, continue
-if (keyList != null && option != null && int.Parse(option) <= keyList.Count())
+
+// var - allows for string or null - saving input from terminal
+var option = Console.ReadLine();
+//While the user has not inputted something valid
+while (true)
 {
-    string valueToUse = keyList[int.Parse(option)];
-    
-    
-    //TODO - save the key and value that the user has chosen and move on to sorting.
+    // Try to catch int.Parse throwing an error when something other then a number is inputted. 
+    try
+    {
+        // If option is not null and input is smaller then or equal to the amount of keys
+        if (option != null && int.Parse(option) <= keyList.Count)
+        {
+            // Break while loop - user has inputted valid option
+            break;
+        }
+        //Tell user to only enter an option
+        else
+        {
+            Console.WriteLine("Please only enter an option above!");
+        }
+    }
+    //Tell the user to only enter a number - error caught from the int.Parse
+    catch (Exception)
+    {
+        Console.WriteLine("Please only enter a number");
+    }
+}
+string fileName = keyList[int.Parse(option)];
+string?[] data = output[keyList[int.Parse(option)]].ToArray();
+
+Console.WriteLine($"File name: {fileName}");
+foreach (var i in data)
+{
+    Console.WriteLine($"data: {i}");
 }
