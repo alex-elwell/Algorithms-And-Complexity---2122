5af9a6e176628dd4464a259f4ee4f6d4ae868280
Alexander Elwell <alexander.hyaden.elwell@gmail.com>
2022-04-26 14:59:07 +0100

    Search.cs updated - both linear and binary search complete

--- Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Program.cs
+++ Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Program.cs
@@ -8,106 +8,109 @@ using Algorithms_And_Complexity___2122;
 // Calling FileReturn and saving the output as 'output' (Dictionary)
 var output = Input.FileReturn();
 
-// Printing the output of the FileReturn()
-foreach (var i in output)
+List<int> masterStepCount = new List<int>();
+
+
+
+
+while (true)
 {
-    Console.WriteLine(i);
-    
-    // checking for the returned values  
-    
-    // foreach (var x in i.Value)
+    //Count for the amount of data 
+    int count = 0;
+    //Outputting instructions to User Terminal
+    Console.WriteLine("Please Choose A file:");
+    //Creating new List that will hold all of the Key's we can use to access the dictionary
+    List<string> keyList = new List<string>();
+    //Loop through the Dictionary from FileReturn (output) 
+    foreach (var i in output)
+    {
+        //Output the options to the user
+        Console.WriteLine(count + " " + i.Key);
+        //Adding the key to to the KeyList to allow us to access Dictionary
+        keyList.Add(i.Key);
+        //Adding to the count / the index of keyList
+        count++;
+    }
+
+    string? option;
+    //While the user has not inputted something valid
+    while (true)
+    {
+        // var - allows for string or null - saving input from terminal
+        option = Console.ReadLine();
+        // Try to catch int.Parse throwing an error when something other then a number is inputted. 
+        try
+        {
+            // If option is not null and input is smaller then or equal to the amount of keys
+            if (option != null && int.Parse(option) <= keyList.Count)
+            {
+                // Break while loop - user has inputted valid option
+                break;
+            }
+            //Tell user to only enter an option
+            else
+            {
+                Console.WriteLine("Please only enter an option above!");
+            }
+        }
+        //Tell the user to only enter a number - error caught from the int.Parse
+        catch (Exception)
+        {
+            Console.WriteLine("Please only enter a number");
+        }
+    }
+
+    //Saving both the fileName and data to the corresponding variable 
+    var fileName = keyList[int.Parse(option)];
+    //array of integers - converting output array dictionary's request data (using key) into an array of integers
+    int[] data = Array.ConvertAll(output[keyList[int.Parse(option)]].ToArray(), s =>
+    {
+        //If s (piece of data / number) is null - Alert error and exit with code 1 - if not null convert to int. 
+        if (s != null) return int.Parse(s);
+        Console.WriteLine("There was an error - please use a different data Source");
+        Environment.Exit(1);
+        return 0;
+    });
+
+    // foreach (var i in data)
     // {
-    //     Console.WriteLine(x);
+    //     Console.WriteLine($"Data: {i}");
+    //     Console.WriteLine($"Data type: {i.GetType()}");
     // }
-}
+    // Console.WriteLine($"File name: {fileName}");
 
-//Count for the amount of data 
-int count = 0;
-//Outputting instructions to User Terminal
-Console.WriteLine("Please Choose A file:");
-//Creating new List that will hold all of the Key's we can use to access the dictionary
-List<string> keyList = new List<string>();
-//Loop through the Dictionary from FileReturn (output) 
-foreach (var i in output)
-{
-    //Output the options to the user
-    Console.WriteLine(count + " " + i.Key);
-    //Adding the key to to the KeyList to allow us to access Dictionary
-    keyList.Add(i.Key);
-    //Adding to the count / the index of keyList
-    count++;
-}
+    var sort = new Sort(data);
 
-// var - allows for string or null - saving input from terminal
-var option = Console.ReadLine();
-//While the user has not inputted something valid
-while (true)
-{
-    // Try to catch int.Parse throwing an error when something other then a number is inputted. 
-    try
+    string? sortDecision;
+    while (true)
     {
-        // If option is not null and input is smaller then or equal to the amount of keys
-        if (option != null && int.Parse(option) <= keyList.Count)
+        Console.WriteLine("Please Select a Sorting Algorithm: ");
+        Console.WriteLine("1: Bubble Sort");
+        Console.WriteLine("2: Quick Sort");
+        Console.WriteLine("3: Insertion Sort");
+        Console.WriteLine("4: Merge Sort- NOP");
+        Console.WriteLine("5: Quit");
+        sortDecision = Console.ReadLine();
+        try
         {
-            // Break while loop - user has inputted valid option
-            break;
+            //this will throw an error if anything but a number is inputted
+            if (sortDecision != null)
+            {
+                int.Parse(sortDecision);
+                break;
+            }
         }
-        //Tell user to only enter an option
-        else
+        catch (Exception)
         {
-            Console.WriteLine("Please only enter an option above!");
+            Console.WriteLine("You should only enter numbers into this field!");
+            continue;
+            // throw;
         }
     }
-    //Tell the user to only enter a number - error caught from the int.Parse
-    catch (Exception)
+
+    if (sortDecision == "5")
     {
-        Console.WriteLine("Please only enter a number");
-    }
-}
-
-//Saving both the fileName and data to the corresponding variable 
-var fileName = keyList[int.Parse(option)];
-//array of integers - converting output array dictionary's request data (using key) into an array of integers
-int[] data = Array.ConvertAll(output[keyList[int.Parse(option)]].ToArray(), s =>
-{
-    //If s (piece of data / number) is null - Alert error and exit with code 1 - if not null convert to int. 
-    if (s != null) return int.Parse(s);
-    Console.WriteLine("There was an error - please use a different data Source");
-    Environment.Exit(1);
-    return 0;
-});
-
-// foreach (var i in data)
-// {
-//     Console.WriteLine($"Data: {i}");
-//     Console.WriteLine($"Data type: {i.GetType()}");
-// }
-// Console.WriteLine($"File name: {fileName}");
-
-var sort = new Sort(data);
-
-
-while (true)
-{
-    Console.WriteLine("Please Select a Sorting Algorithm: ");
-    Console.WriteLine("1: Bubble Sort");
-    Console.WriteLine("2: Quick Sort");
-    Console.WriteLine("3: Selection Sort");
-    Console.WriteLine("4: Merge Sort");
-    string? sortDecision = Console.ReadLine();
-    try
-    {
-        //this will throw an error if anything but a number is inputted
-        if (sortDecision != null)
-        {
-            int.Parse(sortDecision);
-        }
-    }
-    catch (Exception)
-    {
-        Console.WriteLine("You should only enter numbers into this field!");
-        continue;
-        // throw;
+        break;
     }
 
     string? directionOption;
@@ -119,44 +122,135 @@ while (true)
         {
             int.Parse(directionOption!);
         }
-        catch (Exception )
+        catch (Exception)
         {
             Console.WriteLine("Please only enter numbers!");
             continue;
         }
 
-        if (int.Parse(directionOption!) == 1 || int.Parse(directionOption!) == 2 )
+        if (int.Parse(directionOption!) == 1 || int.Parse(directionOption!) == 2)
         {
             break;
         }
-        Console.WriteLine("PLease only enter 1 or 2 ");
+
+        Console.WriteLine("Please only enter 1 or 2 ");
+    }
+
+    int[] dataReturn = new int[] { };
+
+    if (int.Parse(sortDecision!) <= 4)
+    {
+        switch (sortDecision)
+            {
+                case "1":
+                    dataReturn = sort.BubbleSort(int.Parse(directionOption!));
+                    
+                    //adding to the masterStepCount that will hold both the type of search and the amount of steps
+                    masterStepCount.Add(1);
+                    masterStepCount.Add(sort.StepCount);
+                    break;
+                case "2":
+                    int[] dataCopy = new int[data.Length];
+                    Array.Copy(data, dataCopy, data.Length);
+                    dataReturn = sort.QuickSort(dataCopy, int.Parse(directionOption!), 0, data.Length - 1);
+                    //adding to the masterStepCount that will hold both the type of search and the amount of steps
+                    masterStepCount.Add(2);
+                    masterStepCount.Add(sort.StepCount);
+                    break;
+                case "3":
+                    dataReturn = sort.InsertionSort(int.Parse(directionOption!));
+                    //adding to the masterStepCount that will hold both the type of search and the amount of steps
+                    masterStepCount.Add(3);
+                    masterStepCount.Add(sort.StepCount);
+                    break;
+                case "4":
+                    dataReturn = sort.MergeSortMain(int.Parse(directionOption!));
+                    //adding to the masterStepCount that will hold both the type of search and the amount of steps
+                    masterStepCount.Add(4);
+                    masterStepCount.Add(sort.StepCount);
+                    break;
+            }
     }
     
-    if (int.Parse(sortDecision!) <= 4)
-            {
-                switch (sortDecision)
-                {
-                    case "1":
-                        var bubbleSortReturn = sort.BubbleSort(int.Parse(directionOption!));
-                        foreach (var i in bubbleSortReturn)
-                        {
-                            Console.WriteLine(i);
-                        }
-                        break;
-                    case "2":
-                        var quickSortReturn = sort.QuickSort(int.Parse(directionOption!));
-                        Console.WriteLine(quickSortReturn);
-                        break;
-                    case "3":
-                        var selectionSortReturn = sort.SelectionSort(int.Parse(directionOption!));
-                        Console.WriteLine(selectionSortReturn);
-                        break;
-                    case "4":
-                        var mergeSortReturn = sort.MergeSort(int.Parse(directionOption!));
-                        Console.WriteLine(mergeSortReturn);
-                        break;
-                }
-                break;
-            }
+    if (fileName.Contains("2048"))
+    {
+        //Print the 50th value for each thing 
+        Console.WriteLine($"The size of the array is: {dataReturn.Length}");
+        for (int i = 0; i < dataReturn.Length; i = i + 50)
+        {
+            Console.WriteLine($"Index: {i} Data: {dataReturn[i]}");
         }
+    }
+    else if (fileName.Contains("256"))
+    {
+        Console.WriteLine($"The size of the array is: {dataReturn.Length}");
+        for (int i = 0; i < dataReturn.Length; i = i + 10)
+        { 
+            Console.WriteLine($"$index: {i} Data: {dataReturn[i]}");
+        }
+    }
+
+    string? searchOption;
+    while (true)
+    {
+        Console.WriteLine("\n1: Linear Search\n2: Binary Search");
+        searchOption = Console.ReadLine();
+        if (searchOption == "1" || searchOption == "2")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please only enter '1' or '2'");
+        }
+    }
+    
+    //Searching Algorithms
+    var search = new Search();
+    string? query;
+    // search - dataReturn(holding the data to be searched)
+    while (true)
+    {
+        Console.WriteLine("What would you like to Search for?");
+        query = Console.ReadLine();
+        try
+        {
+            int queryInt = int.Parse(query.Trim());
+            //if the parse worked - we just have a number so
+            //we can continue to send this to the search
+            break;
+        }
+        catch (Exception )
+        {
+            Console.WriteLine("Please Only enter a number");
+        }
+    }
+
+
+    if (searchOption == "1")
+    {
+        //THIS COULD RETURN NULL / EMPTY LIST - ALLOW FOR THIS!
+            
+        var linearReturn = search.LinearSearch(dataReturn, int.Parse(query));
+        foreach (var i in linearReturn)
+        {
+            Console.WriteLine(i);
+        }
+    }else if (searchOption == "2")
+    {
+        search.BinarySearch(dataReturn, int.Parse(query));
+    }
+}
+
+
+//TODO - Add option to see steps?
+foreach (var i in masterStepCount)
+{
+    Console.WriteLine(i);
+}
+
+//TODO - ability to combine arrays - 2
+ 
+
+//TODO - Analyse all of the steps for sorts.
 
--- Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Sort.cs
+++ Algorithms And Complexity - 2122/Algorithms And Complexity - 2122/Sort.cs
@@ -11,23 +11,60 @@ public class Sort
     private int[] Data { get; set; }
     public int StepCount { get; set; }
 
-    public int[] QuickSort(int reverse)
+    
+    public int QuickSortStep { get; set; } = 0;
+
+    public int[] QuickSort(int[] data, int reverse, int low, int high )
     {
-        //Quick Sort algorithm!
-        
-        
-        //automatically return array of [null]
-        return new int[1];
+        // Algorithm
+        if (low < high)
+        {
+            int partition = QuickSortPartition(data, low, high);
+            QuickSort(data, reverse, low, partition - 1);
+            QuickSort(data, reverse, partition + 1, high);
+        }
+
+        if (reverse == 2)
+        {
+            //TODO - make this reverse - if not remove option for this and do it one way only!
+        }
+        //return array 
+        return data;
     }
 
+    public int QuickSortPartition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high]; //Pivot - element to be placed right
+        int i = low - 1;
+        for (int j = low; j <= high-1; j++)
+        {
+            QuickSortStep++;
+            if (arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp2 = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp2;
+        return (i+1);
+    }
+    
+    
+    
+    
+    
+    
     public int[] BubbleSort(int reverse)
     {
         int[] dataCopy = new int[Data.Length];
         Array.Copy(Data, dataCopy, Data.Length);
         //Step Counter
         int stepCount = 0;
-        
-        
+
         //Ascending Order 
         if (reverse == 1)
         {
@@ -54,9 +91,9 @@ public class Sort
             int temp;
             for (var y = 0; y <= dataCopy.Length - 2; y++)
             {
-                stepCount++;
                 for (var i = 0; i <= dataCopy.Length - 2; i++)
                 {
+                    stepCount++;
                     if (dataCopy[i] < dataCopy[i + 1])
                     {
                         temp = dataCopy[i + 1];
@@ -65,18 +102,93 @@ public class Sort
                     }
                 }
             }
+
             StepCount = stepCount;
             return dataCopy;
         }
+        
     }
     
-    public int[] SelectionSort(int reverse)
+    public int[] InsertionSort(int reverse)
     {
+        
+        int[] dataCopy = new int[Data.Length];
+        Array.Copy(Data, dataCopy, Data.Length);
+        if (reverse == 1)
+        {
+            //Step Counter
+            int stepCount = 0;
+        
+            //initialising required integers for sort - all 0 except n
+            int i, j, valueToCopy;
+
+            for (i = 1; i < dataCopy.Length; i++)
+            {
+                valueToCopy = dataCopy[i];
+                for (j = i - 1; j >= 0;)
+                {
+                    stepCount++;
+                    if (valueToCopy < dataCopy[j])
+                    {
+                        dataCopy[j + 1] = dataCopy[j];
+                        j--;
+                        dataCopy[j + 1] = valueToCopy;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            StepCount = stepCount;
+            return dataCopy;
+        }
+        else
+        {
+            //Step Counter
+            int stepCount = 0;
+        
+            //initialising required integers for sort - all 0 except n
+            int i, j, valueToCopy;
+
+            for (i = 1; i < dataCopy.Length; i++)
+            {
+                valueToCopy = dataCopy[i];
+                for (j = i - 1; j >= 0;)
+                {
+                    stepCount++;
+                    if (valueToCopy > dataCopy[j])
+                    {
+                        dataCopy[j + 1] = dataCopy[j];
+                        j--;
+                        dataCopy[j + 1] = valueToCopy;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            StepCount = stepCount;       
+            return dataCopy;
+        }
+    }
+    public int[] MergeSortMain(int reverse)
+    {
+        //TODO - merge sort 
+        
+        
+        
+        //Merge Sort 
         return new int[1];
     }
-    
-    public int[] MergeSort(int reverse)
-    {
-        return new int[1];
-    }
+
+    // private int merge(int[] data, int l, int m, int r)
+    // {
+    //     
+    //     return 1;
+    //
+    // }
 }
